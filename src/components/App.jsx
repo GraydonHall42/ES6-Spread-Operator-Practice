8843be34681a6bdad877f4e132565ca1b90e20b7
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-24 14:36:12 -0700

    Made to do list in React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 
 function App() {
+  const [item, setItem] = React.useState("");
+  const [list, setList] = React.useState([]);
+
+  function createNewItem() {
+    const newItem = event.target.value;
+    setItem(newItem);
+  }
+
+  function addItemToList() {
+    setList((prevList) => {
+      return [...prevList, item];
+    });
+    setItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={createNewItem} type="text" value={item} />
+        <button onClick={addItemToList}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {list.map((listItem) => (
+            <li>{listItem}</li>
+          ))}
         </ul>
       </div>
     </div>
